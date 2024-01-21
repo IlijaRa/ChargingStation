f76d6481cc07d1febb788e23521762bb153e3bf0
IlijaRa <ilija.rakocevic@finiti.rs>
2024-01-21 19:45:23 +0100

    alldeleted

--- README.md
+++
@@ -1,2 +0,0 @@
-# ChargingStation.BE
-backend part
--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDbUrl = 'mongodb+srv://root:root@chargingstationsmongodb.gxgr2hi.mongodb.net/charging-stations?retryWrites=true&w=majority';
